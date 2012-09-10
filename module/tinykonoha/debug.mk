1253cf4812bae29aa4387c15f21e9dc26aa5067b
Shunsuke Shida <shunsuke.sida@gmail.com>
2012-09-10 21:15:57 +0900

    update debug.mk

--- module/tinykonoha/debug.mk
+++ module/tinykonoha/debug.mk
@@ -1,4 +1,8 @@
-PACKAGES = ../../package/konoha.float/float_glue.o
+PKGS = ../../package/konoha.float/float_glue\
+	   ../../package/konoha.array/array_glue\
+
+OBJS = float_glue.o\
+	   array_glue.o\
 
 UNAME = tinykonoha
 DEBUG_CC = gcc
@@ -6,11 +10,11 @@ DEBUG_CFLAGS = -g2 -I../../include -I. -I../tinyvm/
 
 all : $(UNAME)
 
-$(UNAME): $(PACKAGES) $(UNAME).o
-	$(DEBUG_CC) $(OBJS) float_glue.o $(UNAME).o -o $(UNAME) $(DEBUG_CFLAGS) -DK_USING_TINYVM=1
+$(UNAME): $(PKGS) $(UNAME).o
+	$(DEBUG_CC) $(OBJS) $(UNAME).o -o $(UNAME) $(DEBUG_CFLAGS) -DK_USING_TINYVM=1
 
 $(UNAME).o: $(UNAME).c
 	$(DEBUG_CC) $(UNAME).c -c $(DEBUG_CFLAGS) -DK_USING_TINYVM=1
 
-$(PACKAGES): %.o: %.c
+$(PKGS): %: %.c
 	$(DEBUG_CC) -c $(DEBUG_CFLAGS) $< -DK_USING_TINYVM=1
